fd217813abb0566b01b6ea6863f065c781b22c35
Far4Mac <far4mac@gmail.com>
2017-07-11 01:33:15 +0300

    added class based views

--- onlinestoreapp/models.py
+++ onlinestoreapp/models.py
@@ -6,16 +6,16 @@ class Category(models.Model):
     name = models.CharField(max_length=50)
     slug = models.SlugField(max_length=100)
 
-    def __str__(self):
+    def __unicode__(self):
         return self.name
 
 class Product(models.Model):
     name = models.CharField(max_length=50)
     category = models.ForeignKey(Category)
     slug = models.SlugField(max_length=100)
-    image = models.ImageField()
+    image = models.ImageField(upload_to='images')
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
 
-    def __str__(self):
+    def __unicode__(self):
         return self.name
--- onlinestoreapp/views.py
+++ onlinestoreapp/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.views.generic import ListView, DetailView
+from .models import Category, Product
 
-# Create your views here.
+
+# all available products
+class Products(ListView):
+    model = Product
+    context_object_name = 'products'
+    template_name = 'product/list.html'
+    paginate_by = 10
+
+    def get_queryset(self):
+        return Product.objects.all()
+
+
+class Products_in_category(Products):
+    def get_queryset(self, category_slug):
+        category = get_object_or_404(Category, slug=category_slug)
+        return super(Products_in_category, self).get_products().exclude(category__ne=category)
+
+    def get_context_data(self, **kwargs):
+        context = super(Products_in_category, self).get_context_data(**kwargs)
+        context['category'] = get_object_or_404(Category, id=self.args[0], slug=self.args[1])
+        return context
+
+
+class ProductDetail(DetailView):
+    model = Product
+    context_object_name = 'product'
+    template_name = 'product/detail.html'
+    queryset = Product.objects.all()
+
+    def get_object(self):
+        return super(ProductDetail, self).get_object()
